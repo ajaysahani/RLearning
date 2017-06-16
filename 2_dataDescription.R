fdb64a1dc05f39131d34656a47073db2a2c0ad24
Ajay <ajaysahani95@gmail.com>
2017-06-16 08:41:52 +0530

    learning data description

---
+++ 2_dataDescription.R
@@ -0,0 +1,18 @@
+#--------------Types of Data----------------------
+# 1_Quantitative data:measurment of quantity of something
+# a:Discrete data= eg: no of arrivals ,no of success :: represented by integers
+# b:Continious data= eg: height,weight,length,time :: represented by decimals
+
+# example
+# 1:Annual Precipitions in US Cities- avg amnt of rainfall for each 70 cities
+# quantitative continious data
+str(precip)
+precip[1:4]
+
+# 2:Lengths of Major North American Rivers
+# quantitative continious data
+str(rivers)
+
+# 3:Yearly no. of important Discoveries
+str(discoveries)
+discoveries[1:4]
