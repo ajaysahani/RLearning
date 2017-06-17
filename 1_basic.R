0c3b13fa3999656cc9f7164de390fa7254330497
Ajay <ajaysahani95@gmail.com>
2017-06-17 22:11:18 +0530

    Added other basic operation

--- 1_basic.R
+++ 1_basic.R
@@ -28,6 +28,10 @@ options(digits = 7)
 #------------Assignment, Object names, and Data types
 x <- 7*4/pi
 x
+a <- numeric(10)
+a
+b <- TRUE
+typeof(b)
 
 # integer: the value 0,1,-1,2,-2,3,-3,........,n,-n
 # double: real numbers
@@ -43,7 +47,7 @@ typeof((0+1i)^2)
 
 
 #--------------------Vector--------------------------
-# c
+# c stands for combine
 x <- c(74, 31, 95, 61, 76, 34, 23, 54,96)
 x
 
@@ -66,7 +70,25 @@ LETTERS[1:5]
 letters[1:5]
 
 
+#-------------Table-------------------------------
+a <- factor(c("A","A","B","A","B","B","C","A","C"))
+results <- table(a)
+results
+
+#-------------------Matrix---------------------------
+m <- matrix(c(4,3,2),ncol=3,byrow=TRUE)
+m
+colnames(m) <- c("A","B","C")
+m
+
+
+
 #-----------Functions and Expressions-----------------
+a = c(2,4,6,3,1,5)
+b = sort(a)
+c = sort(a,decreasing = TRUE)
+c
+
 x<- 1:5
 sum(x)
 length(x)
@@ -77,6 +99,7 @@ sd(x)
 
 help(sd)
 ?sd
+??sd
 
 #type the name of fuction w/o any parameter or argument
 intersect
@@ -101,6 +124,51 @@ new_function <- function(a){
   }
 }
 new_function(6)
+
+#installing and Loading Add-on Packages
+getOption("defaultPackages")
+install.packages("foo",dependencies = TRUE)
+
+#---------------------Logical Data----------------------
+x <- 5:9
+y <- (x<7.3)
+y
+!y
+
+
+#--------------------Missing Data------------------------
+x <- c(3,7,NA,4,7) #NA (Not Applicable)
+y <- c(5,NA,1,2,2)
+x+y
+
+sum(x)
+sum(x, na.rm = TRUE) #ignore NA
+
+is.na(x)
+z <- x[!is.na(x)]
+sum(z)
+
+
+#-------------Data Frames---------------
+x <- 5:8
+y <- letters[3:6]
+A <- data.frame(v1=x, v2=y)
+A
+A[3,]
+A[,2]
+names(A)
+A$v1
+
+
+#--------------Reading from csv file------------------
+h <- read.csv(file.choose(), head=TRUE, sep = ",")
+h
+dir()
+getwd()
+h$trial
+h$mass
+h$velocity
+names(h)
 
 
 
--- 2_dataDescription.R
+++ 2_dataDescription.R
@@ -16,3 +16,94 @@ str(rivers)
 # 3:Yearly no. of important Discoveries
 str(discoveries)
 discoveries[1:4]
+
+
+
+#--------------------Displaying Qunatitive Data--------------
+# Strip Charts(Dot Plots)
+stripchart(precip, xlab = "rainfall")
+str(precip)
+stripchart(rivers, method = "jitter",xlab = "length")
+str(rivers)
+stripchart(discoveries,method = "stack",xlab = "number")
+str(discoveries)
+
+# Histogram (typically used with continious data)
+# Annual Precipitation in US Cities
+hist(precip, main="Histogram of precip")
+hist(precip, freq = FALSE, main = "Histogram of precip")
+#change bins slightly (with break argument to hist)
+hist(precip,breaks = 10,main="")
+hist(precip,breaks = 200,main = "")
+
+# Stemplots(stems and leaves)
+#https://stackoverflow.com/questions/5567500/can-not-load-package-tcltk-in-r
+# UKDriverDeath time series
+install.packages("tcltk",dependencies = TRUE)
+library(aplpack)
+help(aplpack)
+??aplpack
+stem.leaf(UKDriverDeaths,depths = FALSE)
+str(UKDriverDeaths)
+
+# Index Plot
+plot(LakeHuron, type="h")
+str(LakeHuron)
+plot(LakeHuron, type="p")
+
+
+#Qualitative Data, Categorical Data, Factors
+str(state.abb)
+state.abb[1:50]
+str(state.region)
+
+
+#---------------Displaying Qualitaive Data--------------------
+# table
+tbl <- table(state.division)
+tbl
+#relative frequencies
+tbl/sum(tbl)
+#same thing
+prop.table(tbl)
+
+
+# Bar Graph
+barplot(table(state.region),cex.names = 0.6)#cex.names label size
+?state.region
+str(state.region)
+barplot(prop.table(table(state.region)), cex.names = 0.5)
+
+
+# Pareto Diagrams
+install.packages("qcc",dependencies = TRUE)
+library(qcc)
+pareto.chart(table(state.division), ylab = "Frequency")
+
+# Dot Charts
+x <- table(state.region)
+dotchart(as.vector(x),labels = names(x))
+
+
+# Clusters and Gaps
+library(aplpack)
+stem.leaf(faithful$eruptions)
+
+#----------------Outliers-----------------------------
+boxplot.stats(rivers)$out
+boxplot.stats(rivers,coef = 3)$out
+str(rivers)
+
+
+
+#-------------Multivariate Data and Data Frames---------------
+x <- 5:8
+y <- letters[3:6]
+A <- data.frame(v1=x, v2=y)
+A
+A[3,]
+A[,2]
+names(A)
+A$v1
+
+
